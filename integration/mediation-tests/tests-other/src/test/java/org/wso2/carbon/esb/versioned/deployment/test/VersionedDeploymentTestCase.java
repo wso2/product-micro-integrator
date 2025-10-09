1a2ab4744e84a89f7a7dad429c80eed0ff14120d
Sanoj Punchihewa <sanoj.punchihewa@gmail.com>
2025-10-09 22:12:04 +0530

    Update versioned capp test cases

--- integration/mediation-tests/tests-other/src/test/java/org/wso2/carbon/esb/versioned/deployment/test/VersionedDeploymentTestCase.java
+++ integration/mediation-tests/tests-other/src/test/java/org/wso2/carbon/esb/versioned/deployment/test/VersionedDeploymentTestCase.java
@@ -23,6 +23,7 @@ import org.junit.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.wso2.carbon.base.CarbonBaseUtils;
 import org.wso2.esb.integration.common.utils.CarbonLogReader;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 import org.wso2.esb.integration.common.utils.Utils;
@@ -44,9 +45,11 @@ public class VersionedDeploymentTestCase extends ESBIntegrationTest {
     private static final String TEMPLATE_CAPP_NAME = "template_1.0.1.car";
     private static final String PAYMENT_100_CAPP_NAME = "payment_1.0.0.car";
     private static final String PAYMENT_101_CAPP_NAME = "payment_1.0.1.car";
+    private static final String BANKING_SERVICE_CAPP_NAME = "bankingservices_1.0.0.car";
     private static final String paymentAPI100ExpectedPayload = "{\"weather\":{\"main\":\"Clear\",\"description\":\"CLEAR SKY\",\"icon\":\"01d\",\"daylight_hours\":11.743055555555555},\"temp\":{\"temp\":23.760000000000048,\"temp_description\":\"Hot\"},\"wind\":{\"speed\":3.87,\"deg\":39},\"pressure\":1011,\"humidity\":\"87%\",\"visibility\":10000}";
     private static final String paymentAPI101ExpectedPayload = "{\"weather\":{\"main\":\"Rainy\",\"description\":\"LIGHT RAIN\",\"icon\":\"10d\",\"daylight_hours\":11.743055555555555},\"temp\":{\"temp\":23.760000000000048,\"temp_description\":\"Hot\"},\"wind\":{\"speed\":3.87,\"deg\":39},\"pressure\":1011,\"humidity\":\"87%\",\"visibility\":10000}";
     private ServerConfigurationManager serverConfigurationManager;
+    private static final String VERSIONED_CAPP_FOLDER = "versionedDeployment";
 
     @BeforeClass(alwaysRun = true)
     public void uploadSynapseConfig() throws Exception {
@@ -56,22 +59,33 @@ public class VersionedDeploymentTestCase extends ESBIntegrationTest {
         carbonLogReader = new CarbonLogReader();
         carbonLogReader.start();
 
+        // Set env variables for linux and mac
+        File newSynapseProps = new File(
+                getESBResourceLocation() + File.separator + VERSIONED_CAPP_FOLDER + File.separator + "synapse.properties");
+        File oldSynapseProps =
+                new File(CarbonBaseUtils.getCarbonHome() + File.separator + "conf" + File.separator + "synapse.properties");
+        serverConfigurationManager.applyConfigurationWithoutRestart(newSynapseProps, oldSynapseProps, true);
+
+        serverConfigurationManager.restartMicroIntegrator();
+
         File templateCAPP = new File(
-                getESBResourceLocation() + File.separator + "versionedDeployment" + File.separator +
+                getESBResourceLocation() + File.separator + VERSIONED_CAPP_FOLDER + File.separator +
                         TEMPLATE_CAPP_NAME);
         serverConfigurationManager.copyToCarbonapps(templateCAPP);
         assertTrue(Utils.checkForLog(carbonLogReader, "Successfully Deployed Carbon Application : " +
                 "com.microintegrator.projects__template__1.0.1", 20), "Failed to deploy template car");
 
         File payment100CAPP = new File(
-                getESBResourceLocation() + File.separator + "versionedDeployment" + File.separator +
+                getESBResourceLocation() + File.separator + VERSIONED_CAPP_FOLDER + File.separator +
                         PAYMENT_100_CAPP_NAME);
         serverConfigurationManager.copyToCarbonapps(payment100CAPP);
+        assertTrue(Utils.checkForLog(carbonLogReader, "Initializing API: " +
+                "com.microintegrator.projects__payment__1.0.0__PaymentAPI", 20), "Failed to deploy versioned API");
         assertTrue(Utils.checkForLog(carbonLogReader, "Successfully Deployed Carbon Application : " +
                 "com.microintegrator.projects__payment__1.0.0", 20), "Failed to deploy template car");
 
         File payment101CAPP = new File(
-                getESBResourceLocation() + File.separator + "versionedDeployment" + File.separator +
+                getESBResourceLocation() + File.separator + VERSIONED_CAPP_FOLDER + File.separator +
                         PAYMENT_101_CAPP_NAME);
         serverConfigurationManager.copyToCarbonapps(payment101CAPP);
         assertTrue(Utils.checkForLog(carbonLogReader, "Successfully Deployed Carbon Application : " +
@@ -87,28 +101,111 @@ public class VersionedDeploymentTestCase extends ESBIntegrationTest {
         SimpleHttpClient httpClient = new SimpleHttpClient();
         HttpResponse httpResponse1 = httpClient.doGet(paymentAPI100URL, headers);
 
-        Assert.assertEquals(httpResponse1.getStatusLine().getStatusCode(), 200);
+        Assert.assertEquals(200, httpResponse1.getStatusLine().getStatusCode());
         // Assert the JSON payloads
         org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse1), Object.class),
                 new Gson().fromJson(paymentAPI100ExpectedPayload, Object.class));
 
         HttpResponse httpResponse2 = httpClient.doGet(paymentAPI101URL, headers);
 
-        Assert.assertEquals(httpResponse2.getStatusLine().getStatusCode(), 200);
+        Assert.assertEquals(200, httpResponse2.getStatusLine().getStatusCode());
         // Assert the JSON payloads
         org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse2), Object.class),
                 new Gson().fromJson(paymentAPI101ExpectedPayload, Object.class));
 
         carbonLogReader.checkForLog("Log from seq from local entry v1.0.1", DEFAULT_TIMEOUT);
-        carbonLogReader.stop();
+    }
+
+    @Test(groups = "wso2.esb", description = "Testcase to test versioned deployment for connectors")
+    public void testVersionedDeploymentConnectors() throws Exception {
+
+        String seqTemplateAPI = getMainSequenceURL() + "/com.microintegrator.projects/payment/1.0.0/mockapi";
+        String httpConnectorAPI = getMainSequenceURL() + "/com.microintegrator.projects/payment/1.0.0/mockapi/test";
+        Map<String, String> headers = new HashMap<>(1);
+        SimpleHttpClient httpClient = new SimpleHttpClient();
+        HttpResponse httpResponse1 = httpClient.doGet(seqTemplateAPI, headers);
+
+        Assert.assertEquals(200, httpResponse1.getStatusLine().getStatusCode());
+        String seqTemplateExpectedPayload = "{\"messageID\": \"123\",\"data\": \"Hello,World\"}";
+        org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse1), Object.class),
+                new Gson().fromJson(seqTemplateExpectedPayload, Object.class));
+
+        HttpResponse httpResponse2 = httpClient.doGet(httpConnectorAPI, headers);
+        Assert.assertEquals(200, httpResponse2.getStatusLine().getStatusCode());
+        String httpConnectorExpectedPayload = "{\"time\":\"10:00:76\",\"data\": {\"messageID\":\"123\",\"data\":\"Hello,World\"}}";
+        org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse2), Object.class),
+                new Gson().fromJson(httpConnectorExpectedPayload, Object.class));
+    }
+
+    @Test(groups = "wso2.esb", description = "Testcase to test non versioned deployment when versioned service expose is true",
+    dependsOnMethods = "testVersionedDeploymentConnectors")
+    public void testNonVersionedDeploymentWithServiceExpose() throws Exception {
+
+        File bankingService = new File(
+                getESBResourceLocation() + File.separator + VERSIONED_CAPP_FOLDER + File.separator +
+                        BANKING_SERVICE_CAPP_NAME);
+        serverConfigurationManager.copyToCarbonapps(bankingService);
+
+        carbonLogReader.checkForLog("The ProxyService 'BankProxy' is not included in a versioned CApp. " +
+                "Versioned service exposure is skipped; deploying as a regular ProxyService.", DEFAULT_TIMEOUT);
+        carbonLogReader.checkForLog("The API 'BankAPI' is not included in a versioned CApp. " +
+                "Versioned service exposure is skipped; deploying as a regular API.", DEFAULT_TIMEOUT);
+
+        carbonLogReader.checkForLog("Successfully Deployed Carbon Application : bankingservices_1.0.0", DEFAULT_TIMEOUT);
+
+        String bankAPI = getMainSequenceURL() + "/bankapi";
+        Map<String, String> headers = new HashMap<>(1);
+        SimpleHttpClient httpClient = new SimpleHttpClient();
+        HttpResponse httpResponse1 = httpClient.doGet(bankAPI, headers);
+
+        Assert.assertEquals(200, httpResponse1.getStatusLine().getStatusCode());
+        // Assert the JSON payloads
+        String expectedAPI = "{\"message\":\"Welcome to ABC Bank API\"}";
+        org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse1), Object.class),
+                new Gson().fromJson(expectedAPI, Object.class));
+
+        String bankProxy = getMainSequenceURL() + "/services/BankProxy";
+        httpResponse1 = httpClient.doGet(bankProxy, headers);
+
+        Assert.assertEquals(200, httpResponse1.getStatusLine().getStatusCode());
+        // Assert the JSON payloads
+        String expectedProxy = "{\"message\":\"Welcome to ABC Bank Proxy\"}";
+        org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse1), Object.class),
+                new Gson().fromJson(expectedProxy, Object.class));
+    }
+
+    @Test(groups = "wso2.esb", description = "Testcase to test versioned deployment when versioned service expose is false",
+          dependsOnMethods = "testNonVersionedDeploymentWithServiceExpose")
+    public void testVersionedDeploymentWithoutServiceExpose() throws Exception {
+
+        serverConfigurationManager.restoreToLastConfiguration(true);
+        carbonLogReader.checkForLog("Duplicate resource definition by the name: PaymentAPI", DEFAULT_TIMEOUT);
+
+        // Remove Payment CApp v1.0.1 and restart
+        serverConfigurationManager.removeFromCarbonapps(PAYMENT_101_CAPP_NAME);
+        serverConfigurationManager.restartMicroIntegrator();
+        carbonLogReader.checkForLog("Initializing API: PaymentAPI", DEFAULT_TIMEOUT);
+        carbonLogReader.checkForLog("Successfully Deployed Carbon Application : com.microintegrator.projects__payment__1.0.0", DEFAULT_TIMEOUT);
+
+        String paymentAPI100URL = getMainSequenceURL() + "/paymentapi";
+        Map<String, String> headers = new HashMap<>(1);
+        SimpleHttpClient httpClient = new SimpleHttpClient();
+        HttpResponse httpResponse1 = httpClient.doGet(paymentAPI100URL, headers);
+
+        Assert.assertEquals(200, httpResponse1.getStatusLine().getStatusCode());
+        // Assert the JSON payloads
+        org.testng.Assert.assertEquals(new Gson().fromJson(SimpleHttpClient.responseEntityBodyToString(httpResponse1), Object.class),
+                new Gson().fromJson(paymentAPI100ExpectedPayload, Object.class));
     }
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
 
+        carbonLogReader.stop();
         serverConfigurationManager.removeFromCarbonapps(PAYMENT_101_CAPP_NAME);
         serverConfigurationManager.removeFromCarbonapps(PAYMENT_100_CAPP_NAME);
         serverConfigurationManager.removeFromCarbonapps(TEMPLATE_CAPP_NAME);
+        serverConfigurationManager.removeFromCarbonapps(BANKING_SERVICE_CAPP_NAME);
         super.cleanup();
     }
 }
