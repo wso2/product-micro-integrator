c333df8c150da3ffdaea5aeba7736a93402d11b0
dilhasha <dilhasha.uom@gmail.com>
2025-10-30 22:55:58 +0530

    Add updates to ICP agent
    Add heartbeat updates for new ICP
    
    Add status and tracing enable/disable APIs
    
    Add support for task status change
    
    Add status of task to heartbeat
    
    Add capp information per artifact to the heartbeat
    
    Add support to update statistics status

---
+++ components/org.wso2.micro.integrator.extensions/org.wso2.micro.integrator.management.apis/src/main/java/org/wso2/micro/integrator/management/apis/ICPGetLocalEntryValueResource.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2025, WSO2 Inc.
+ */
+package org.wso2.micro.integrator.management.apis;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.config.SynapseConfiguration;
+import org.apache.synapse.config.Entry;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.json.JSONObject;
+import org.wso2.carbon.inbound.endpoint.internal.http.api.APIResource;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * ICP resource to fetch the value of a given Local Entry.
+ * Endpoint: /icp/artifacts/local-entry?name=<key>
+ */
+public class ICPGetLocalEntryValueResource extends APIResource {
+
+    private static final Log LOG = LogFactory.getLog(ICPGetLocalEntryValueResource.class);
+
+    public ICPGetLocalEntryValueResource(String urlTemplate) {
+        super(urlTemplate);
+    }
+
+    @Override
+    public Set<String> getMethods() {
+        Set<String> methods = new HashSet<>();
+        methods.add(Constants.HTTP_GET);
+        return methods;
+    }
+
+    @Override
+    public boolean invoke(MessageContext messageContext) {
+        buildMessage(messageContext);
+
+        org.apache.axis2.context.MessageContext axisMsgCtx =
+                ((Axis2MessageContext) messageContext).getAxis2MessageContext();
+
+        if (!messageContext.isDoingGET()) {
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Only GET method is supported", axisMsgCtx, Constants.BAD_REQUEST));
+            return true;
+        }
+
+        String name = Utils.getQueryParameter(messageContext, "name");
+        if (StringUtils.isBlank(name)) {
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Missing required parameter: name", axisMsgCtx, Constants.BAD_REQUEST));
+            return true;
+        }
+
+        SynapseConfiguration synapseConfig = messageContext.getConfiguration();
+        if (synapseConfig == null) {
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Synapse configuration is not available", axisMsgCtx,
+                            Constants.INTERNAL_SERVER_ERROR));
+            return true;
+        }
+
+        Entry entry = synapseConfig.getDefinedEntries().get(name);
+        if (entry == null) {
+            axisMsgCtx.setProperty(Constants.HTTP_STATUS_CODE, Constants.NOT_FOUND);
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Local entry not found: " + name, axisMsgCtx, Constants.NOT_FOUND));
+            return true;
+        }
+
+        try {
+            switch (entry.getType()) {
+                case Entry.INLINE_TEXT:
+                case Entry.INLINE_XML: {
+                    JSONObject resp = new JSONObject();
+                    resp.put(Constants.NAME, name);
+                    resp.put("value", Objects.toString(entry.getValue(), ""));
+                    Utils.setJsonPayLoad(axisMsgCtx, resp);
+                    return true;
+                }
+                case Entry.URL_SRC: {
+                    String content = fetchFromUrl(Objects.toString(entry.getValue(), ""));
+                    JSONObject resp = new JSONObject();
+                    resp.put(Constants.NAME, name);
+                    resp.put("value", content);
+                    Utils.setJsonPayLoad(axisMsgCtx, resp);
+                    return true;
+                }
+                case Entry.REMOTE_ENTRY: {
+                    JSONObject resp = new JSONObject();
+                    resp.put(Constants.NAME, name);
+                    resp.put("registryKey", Objects.toString(entry.getValue(), ""));
+                    Utils.setJsonPayLoad(axisMsgCtx, resp);
+                    return true;
+                }
+                default: {
+                    JSONObject resp = new JSONObject();
+                    resp.put(Constants.NAME, name);
+                    resp.put("value", Objects.toString(entry.getValue(), ""));
+                    Utils.setJsonPayLoad(axisMsgCtx, resp);
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to read local entry value for: " + name, e);
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Failed to read local entry value", axisMsgCtx,
+                            Constants.INTERNAL_SERVER_ERROR));
+            return true;
+        }
+    }
+
+    private String fetchFromUrl(String urlStr) throws Exception {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+            return sb.toString();
+        }
+    }
+}
---
+++ components/org.wso2.micro.integrator.extensions/org.wso2.micro.integrator.management.apis/src/main/java/org/wso2/micro/integrator/management/apis/WsdlResource.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2025, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.micro.integrator.management.apis;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.util.AXIOMUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.apache.axis2.engine.AxisConfiguration;
+import org.wso2.carbon.inbound.endpoint.internal.http.api.APIResource;
+import org.wso2.micro.integrator.core.services.CarbonServerConfigurationService;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * ICP resource to fetch WSDL for a given data service or proxy service.
+ * Endpoints:
+ *  - /icp/artifacts/wsdl?service={dataServiceName}
+ *  - /icp/artifacts/wsdl?proxy={proxyServiceName}
+ */
+public class WsdlResource extends APIResource {
+
+    private static final Log LOG = LogFactory.getLog(WsdlResource.class);
+
+    private static final String PARAM_SERVICE = "service";
+    private static final String PARAM_PROXY = "proxy";
+
+    public WsdlResource(String urlTemplate) {
+        super(urlTemplate);
+    }
+
+    @Override
+    public Set<String> getMethods() {
+        Set<String> methods = new HashSet<>();
+        methods.add(Constants.HTTP_GET);
+        return methods;
+    }
+
+    @Override
+    public boolean invoke(MessageContext messageContext) {
+        buildMessage(messageContext);
+
+        org.apache.axis2.context.MessageContext axisMsgCtx = ((Axis2MessageContext) messageContext)
+                .getAxis2MessageContext();
+
+        if (!messageContext.isDoingGET()) {
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Only GET method is supported", axisMsgCtx, Constants.BAD_REQUEST));
+            return true;
+        }
+
+        String serviceName = Utils.getQueryParameter(messageContext, PARAM_SERVICE);
+        String proxyName = Utils.getQueryParameter(messageContext, PARAM_PROXY);
+        if ((serviceName == null || serviceName.trim().isEmpty())
+                && (proxyName == null || proxyName.trim().isEmpty())) {
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Missing required parameter: service or proxy", axisMsgCtx,
+                            Constants.BAD_REQUEST));
+            return true;
+        }
+
+        final boolean isProxyRequest = (proxyName != null && !proxyName.trim().isEmpty());
+        final String targetName = isProxyRequest ? proxyName : serviceName;
+
+        // Validate that the specified Axis service exists (data service or proxy are Axis services)
+        AxisConfiguration axisConfiguration = axisMsgCtx.getConfigurationContext().getAxisConfiguration();
+        org.apache.axis2.description.AxisService axisService = null;
+        if (axisConfiguration != null) {
+            try {
+                axisService = axisConfiguration.getServiceForActivation(targetName);
+                if (axisService == null) {
+                    axisService = axisConfiguration.getService(targetName);
+                }
+            } catch (Exception ignored) {
+            }
+        }
+        if (Objects.isNull(axisService)) {
+            axisMsgCtx.setProperty(Constants.HTTP_STATUS_CODE, Constants.NOT_FOUND);
+            String kind = isProxyRequest ? "Proxy service" : "Data service";
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError(kind + " not found: " + targetName, axisMsgCtx, Constants.NOT_FOUND));
+            return true;
+        }
+
+        try {
+            OMElement wsdl = null;
+
+            // Try to fetch actual WSDL from service URL derived via internal config
+            String wsdlUrl = buildServiceWsdlUrl(axisMsgCtx, targetName);
+            String wsdlText = fetchUrl(wsdlUrl, 5000);
+            
+            if (wsdlText != null && !wsdlText.trim().isEmpty()) {
+                try {
+                    wsdl = AXIOMUtil.stringToOM(wsdlText);
+                } catch (Exception e) {
+                    LOG.error("Failed to parse WSDL XML from URL: " + wsdlUrl, e);
+                    Utils.setJsonPayLoad(axisMsgCtx,
+                            Utils.createJsonError("Failed to parse WSDL XML: " + e.getMessage(),
+                                    axisMsgCtx, Constants.INTERNAL_SERVER_ERROR));
+                    return true;
+                }
+            } else {
+                // WSDL fetch failed or returned empty response
+                String kind = isProxyRequest ? "proxy service" : "data service";
+                Utils.setJsonPayLoad(axisMsgCtx,
+                        Utils.createJsonError("Unable to fetch WSDL for " + kind + ": " + targetName + 
+                                ". Service may not be running or WSDL URL is not accessible.",
+                                axisMsgCtx, Constants.INTERNAL_SERVER_ERROR));
+                return true;
+            }
+
+            // Set XML payload
+            if (axisMsgCtx.getEnvelope() == null) {
+                axisMsgCtx.setEnvelope(OMAbstractFactory.getSOAP11Factory().createSOAPEnvelope());
+                axisMsgCtx.getEnvelope().addChild(OMAbstractFactory.getSOAP11Factory().createSOAPBody());
+            }
+            axisMsgCtx.getEnvelope().getBody().addChild(wsdl);
+
+            axisMsgCtx.setProperty(Constants.MESSAGE_TYPE, "application/xml");
+            axisMsgCtx.setProperty(Constants.CONTENT_TYPE, "application/xml");
+            axisMsgCtx.removeProperty(Constants.NO_ENTITY_BODY);
+
+        } catch (Exception e) {
+            LOG.error("Error fetching WSDL for " + (isProxyRequest ? "proxy" : "data service") + ": " + targetName, e);
+            Utils.setJsonPayLoad(axisMsgCtx,
+                    Utils.createJsonError("Error fetching WSDL: " + e.getMessage(),
+                            axisMsgCtx, Constants.INTERNAL_SERVER_ERROR));
+        }
+
+        return true;
+    }
+
+    private String buildServiceWsdlUrl(org.apache.axis2.context.MessageContext axisMsgCtx, String serviceName) {
+        String host = deriveHost(axisMsgCtx);
+        int port = deriveHttpPort();
+        String protocol = "http";
+        return protocol + "://" + host + ":" + port + "/services/" + serviceName + "?wsdl";
+    }
+
+    private String deriveHost(org.apache.axis2.context.MessageContext axisMsgCtx) {
+        String host = null;
+        if (axisMsgCtx != null && axisMsgCtx.getConfigurationContext() != null &&
+                axisMsgCtx.getConfigurationContext().getAxisConfiguration() != null &&
+                axisMsgCtx.getConfigurationContext().getAxisConfiguration().getParameter("hostname") != null) {
+            Object val = axisMsgCtx.getConfigurationContext().getAxisConfiguration().getParameter("hostname")
+                    .getValue();
+            if (val != null) {
+                host = val.toString();
+            }
+        }
+        if (host == null) {
+            String localIp = System.getProperty("carbon.local.ip");
+            if (localIp != null && !localIp.isEmpty()) {
+                host = localIp;
+            }
+        }
+        if (host == null) {
+            host = "localhost";
+        }
+        return host;
+    }
+
+    private int deriveHttpPort() {
+        String portStr = CarbonServerConfigurationService.getInstance()
+                .getFirstProperty(CarbonServerConfigurationService.HTTP_PORT);
+        int port = 8290;
+        try {
+            if (portStr != null) {
+                port = Integer.parseInt(portStr.trim());
+            }
+        } catch (Exception ignored) {
+        }
+        return port;
+    }
+
+    private String fetchUrl(String urlStr, int timeoutMs) {
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(urlStr);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(timeoutMs);
+            conn.setReadTimeout(timeoutMs);
+            int code = conn.getResponseCode();
+            if (code >= 200 && code < 300) {
+                try (InputStream in = conn.getInputStream()) {
+                    byte[] buf = in.readAllBytes();
+                    return new String(buf, StandardCharsets.UTF_8);
+                }
+            } else {
+                LOG.warn("WSDL fetch returned non-2xx: " + code + " for URL: " + urlStr);
+                return null;
+            }
+        } catch (Exception e) {
+            LOG.warn("Failed to fetch WSDL from URL: " + urlStr + ", " + e.getMessage());
+            return null;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
+}
