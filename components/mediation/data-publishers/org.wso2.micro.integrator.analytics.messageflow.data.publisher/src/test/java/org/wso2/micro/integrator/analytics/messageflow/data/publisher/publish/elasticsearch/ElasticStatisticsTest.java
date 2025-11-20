7cba34eda148f4fc79ccb7cfb6e6263522ec7771
ShammiL <shammi0107@gmail.com>
2025-11-20 20:18:38 +0530

    Add inbound endpoint tests in ELK publishing

--- components/mediation/data-publishers/org.wso2.micro.integrator.analytics.messageflow.data.publisher/src/test/java/org/wso2/micro/integrator/analytics/messageflow/data/publisher/publish/elasticsearch/ElasticStatisticsTest.java
+++ components/mediation/data-publishers/org.wso2.micro.integrator.analytics.messageflow.data.publisher/src/test/java/org/wso2/micro/integrator/analytics/messageflow/data/publisher/publish/elasticsearch/ElasticStatisticsTest.java
@@ -57,6 +57,7 @@ import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.transport.netty.BridgeConstants;
 import org.apache.synapse.transport.nhttp.NhttpConstants;
 import org.wso2.micro.integrator.analytics.messageflow.data.publisher.producer.AnalyticsCustomDataProvider;
+import org.wso2.micro.integrator.analytics.messageflow.data.publisher.publish.PublisherTestUtils;
 import org.wso2.micro.integrator.analytics.messageflow.data.publisher.publish.elasticsearch.schema.ElasticDataSchema;
 
 import javax.xml.stream.XMLStreamException;
@@ -68,25 +69,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ElasticStatisticsTest extends TestCase {
-    private static final String SERVER_INFO_SERVER_NAME = "wso2.dev";
-    private static final String SERVER_INFO_HOST_NAME = "dev.local";
-    private static final String SERVER_INFO_IP_ADDRESS = "1.2.3.4";
-    private static final String SERVER_INFO_PUBLISHER_ID = "WSO2_UNIT_TEST";
-
-    private static final String TEST_API_NAME = "TestAPI";
-    private static final String TEST_API_CONTEXT = "/test";
-    private static final String TEST_API_URL = "/test/admin?search=wso2";
-    private static final String TEST_API_METHOD = "GET";
-    private static final String TEST_API_PROTOCOL = "https";
-    private static final String TEST_SEQUENCE_NAME = "TestSequenceName";
-    private static final String TEST_PROXY_SERVICE = "TestProxyServiceName";
-    private static final String TEST_ENDPOINT_NAME = "TestEndpointName";
-    private static final String TEST_REMOTE_HOST = "127.0.0.1";
-    private static final String TEST_CONTENT_TYPE = "application/json";
-    private static final String TEST_FLOW_ID = "WSO2_UT_00001";
-    private static final int STATIC_LATENCY = 5678;
-    private static final int TENANT_ID = 1234;
-
     private static final int CURRENT_SCHEMA_VERSION = 1;
     private final TestElasticStatisticsPublisher publisher = new TestElasticStatisticsPublisher();
     ServerConfigurationInformation sysConfig = null;
@@ -102,7 +84,7 @@ public class ElasticStatisticsTest extends TestCase {
                 testConfig);
         Axis2MessageContext synCtx;
         org.apache.axis2.context.MessageContext mc = new org.apache.axis2.context.MessageContext();
-        mc.setIncomingTransportName(TEST_API_PROTOCOL);
+        mc.setIncomingTransportName(PublisherTestUtils.TEST_API_PROTOCOL);
         synCtx = new Axis2MessageContext(mc, testConfig, synEnv);
 
         XMLStreamReader parser = StAXUtils.createXMLStreamReader(new StringReader("<test>value</test>"));
@@ -118,57 +100,57 @@ public class ElasticStatisticsTest extends TestCase {
         body.addChild(bodyElement);
         synCtx.setEnvelope(envelope);
 
-        String url = TEST_API_URL;
+        String url = PublisherTestUtils.TEST_API_URL;
 
-        synCtx.setProperty(Constants.Configuration.HTTP_METHOD, TEST_API_METHOD);
-        synCtx.setProperty(BridgeConstants.REMOTE_HOST, TEST_REMOTE_HOST);
-        synCtx.setProperty(BridgeConstants.CONTENT_TYPE_HEADER, TEST_CONTENT_TYPE);
+        synCtx.setProperty(Constants.Configuration.HTTP_METHOD, PublisherTestUtils.TEST_API_METHOD);
+        synCtx.setProperty(BridgeConstants.REMOTE_HOST, PublisherTestUtils.TEST_REMOTE_HOST);
+        synCtx.setProperty(BridgeConstants.CONTENT_TYPE_HEADER, PublisherTestUtils.TEST_CONTENT_TYPE);
         synCtx.setProperty(NhttpConstants.REST_URL_POSTFIX, url.substring(1));
 
         Axis2MessageContext axisCtx = synCtx;
-        axisCtx.getAxis2MessageContext().setProperty(Constants.Configuration.HTTP_METHOD, TEST_API_METHOD);
+        axisCtx.getAxis2MessageContext().setProperty(Constants.Configuration.HTTP_METHOD, PublisherTestUtils.TEST_API_METHOD);
         axisCtx.getAxis2MessageContext().setProperty(
-                Constants.Configuration.TRANSPORT_IN_URL, "https://" + SERVER_INFO_HOST_NAME + url);
+                Constants.Configuration.TRANSPORT_IN_URL, "https://" + PublisherTestUtils.SERVER_INFO_HOST_NAME + url);
         return synCtx;
     }
 
     private void setupSynapseConfig(SynapseConfiguration synapseConfig) throws XMLStreamException {
-        API api = new API(TEST_API_NAME, TEST_API_CONTEXT);
+        API api = new API(PublisherTestUtils.TEST_API_NAME, PublisherTestUtils.TEST_API_CONTEXT);
         Resource resource = new Resource();
         api.addResource(resource);
         synapseConfig.addAPI(api.getName(), api);
 
         SequenceMediator sequence = new SequenceMediator();
         sequence.setSequenceType(SequenceType.NAMED);
-        sequence.setName(TEST_SEQUENCE_NAME);
-        synapseConfig.addSequence(TEST_SEQUENCE_NAME, sequence);
+        sequence.setName(PublisherTestUtils.TEST_SEQUENCE_NAME);
+        synapseConfig.addSequence(PublisherTestUtils.TEST_SEQUENCE_NAME, sequence);
 
         HTTPEndpointFactory factory = new HTTPEndpointFactory();
         OMElement em = AXIOMUtil.stringToOM(
                 "<endpoint><http method=\"GET\" uri-template=\"https://wso2.com\"/></endpoint>");
         EndpointDefinition endpoint = factory.createEndpointDefinition(em);
         HTTPEndpoint httpEndpoint = new HTTPEndpoint();
-        httpEndpoint.setName(TEST_ENDPOINT_NAME);
-        httpEndpoint.setHttpMethod(TEST_API_METHOD);
+        httpEndpoint.setName(PublisherTestUtils.TEST_ENDPOINT_NAME);
+        httpEndpoint.setHttpMethod(PublisherTestUtils.TEST_API_METHOD);
         httpEndpoint.setDefinition(endpoint);
         httpEndpoint.setUriTemplate(UriTemplate.fromTemplate("https://wso2.com"));
-        synapseConfig.addEndpoint(TEST_ENDPOINT_NAME, httpEndpoint);
+        synapseConfig.addEndpoint(PublisherTestUtils.TEST_ENDPOINT_NAME, httpEndpoint);
         httpEndpoint.init(synapseEnvironment);
 
-        ProxyService proxyService = new ProxyService(TEST_PROXY_SERVICE);
-        synapseConfig.addProxyService(TEST_PROXY_SERVICE, proxyService);
+        ProxyService proxyService = new ProxyService(PublisherTestUtils.TEST_PROXY_SERVICE);
+        synapseConfig.addProxyService(PublisherTestUtils.TEST_PROXY_SERVICE, proxyService);
     }
 
-    private void oneTimeSetup() throws Exception {
+    public void oneTimeSetup() throws Exception {
         if (oneTimeSetupComplete) {
             return;
         }
 
         sysConfig = new ServerConfigurationInformation();
-        sysConfig.setServerName(SERVER_INFO_SERVER_NAME);
-        sysConfig.setHostName(SERVER_INFO_HOST_NAME);
-        sysConfig.setIpAddress(SERVER_INFO_IP_ADDRESS);
-        ElasticDataSchema.setPublisherId(SERVER_INFO_PUBLISHER_ID);
+        sysConfig.setServerName(PublisherTestUtils.SERVER_INFO_SERVER_NAME);
+        sysConfig.setHostName(PublisherTestUtils.SERVER_INFO_HOST_NAME);
+        sysConfig.setIpAddress(PublisherTestUtils.SERVER_INFO_IP_ADDRESS);
+        ElasticDataSchema.setPublisherId(PublisherTestUtils.SERVER_INFO_PUBLISHER_ID);
         ElasticDataSchema.setupServerMetadata(sysConfig);
         ConfigurationContext axis2ConfigurationContext = new ConfigurationContext(new AxisConfiguration());
         axis2ConfigurationContext.getAxisConfiguration().addParameter(SynapseConstants.SYNAPSE_ENV, synapseEnvironment);
@@ -192,27 +174,27 @@ public class ElasticStatisticsTest extends TestCase {
 
     public void testPublisherEnabledState() {
         PublishingFlow flow = new PublishingFlow();
-        flow.addEvent(createPublishingEvent(ComponentType.SEQUENCE, TEST_SEQUENCE_NAME));
+        flow.addEvent(createPublishingEvent(ComponentType.SEQUENCE, PublisherTestUtils.TEST_SEQUENCE_NAME));
         publisher.enableService();
-        publisher.process(flow, TENANT_ID);
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
         assertTrue(publisher.isEnabled());
         assertEquals(1, publisher.getAnalyticsCount());
         verifySchema(publisher.getAnalyticData(), AnalyticPayloadType.NON_STANDARD);
         publisher.reset();
         publisher.disableService();
-        publisher.process(flow, TENANT_ID);
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
         assertEquals(0, publisher.getAnalyticsCount());
     }
 
     public void testSequenceAnalytics() {
         PublishingFlow flow = new PublishingFlow();
-        flow.addEvent(createPublishingEvent(ComponentType.SEQUENCE, TEST_SEQUENCE_NAME));
-        publisher.process(flow, TENANT_ID);
+        flow.addEvent(createPublishingEvent(ComponentType.SEQUENCE, PublisherTestUtils.TEST_SEQUENCE_NAME));
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
         assertEquals(1, publisher.getAnalyticsCount());
         verifySchema(publisher.getAnalyticData(), AnalyticPayloadType.SEQUENCE);
 
         for (int i = 0; i < 100; ++i) {
-            publisher.process(flow, TENANT_ID);
+            publisher.process(flow, PublisherTestUtils.TENANT_ID);
         }
         assertEquals(100, publisher.getAnalyticsCount());
     }
@@ -221,38 +203,50 @@ public class ElasticStatisticsTest extends TestCase {
         RestRequestHandler handler = new RestRequestHandler();
         handler.process(messageContext);
         PublishingFlow flow = new PublishingFlow();
-        flow.addEvent(createPublishingEvent(ComponentType.API, TEST_API_NAME));
-        publisher.process(flow, TENANT_ID);
+        flow.addEvent(createPublishingEvent(ComponentType.API, PublisherTestUtils.TEST_API_NAME));
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
         assertEquals(1, publisher.getAnalyticsCount());
         verifySchema(publisher.getAnalyticData(), AnalyticPayloadType.API);
 
         for (int i = 0; i < 100; ++i) {
-            publisher.process(flow, TENANT_ID);
+            publisher.process(flow, PublisherTestUtils.TENANT_ID);
         }
         assertEquals(100, publisher.getAnalyticsCount());
     }
 
     public void testEndpointAnalytics() {
         PublishingFlow flow = new PublishingFlow();
-        flow.addEvent(createPublishingEvent(ComponentType.ENDPOINT, TEST_ENDPOINT_NAME));
-        publisher.process(flow, TENANT_ID);
+        flow.addEvent(createPublishingEvent(ComponentType.ENDPOINT, PublisherTestUtils.TEST_ENDPOINT_NAME));
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
         assertEquals(1, publisher.getAnalyticsCount());
         verifySchema(publisher.getAnalyticData(), AnalyticPayloadType.ENDPOINT);
 
         for (int i = 0; i < 100; ++i) {
-            publisher.process(flow, TENANT_ID);
+            publisher.process(flow, PublisherTestUtils.TENANT_ID);
         }
         assertEquals(100, publisher.getAnalyticsCount());
     }
 
     public void testProxyServiceAnalytics() {
         PublishingFlow flow = new PublishingFlow();
-        flow.addEvent(createPublishingEvent(ComponentType.PROXYSERVICE, TEST_PROXY_SERVICE));
-        publisher.process(flow, TENANT_ID);
+        flow.addEvent(createPublishingEvent(ComponentType.PROXYSERVICE, PublisherTestUtils.TEST_PROXY_SERVICE));
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
         verifySchema(publisher.getAnalyticData(), AnalyticPayloadType.PROXY_SERVICE);
 
         for (int i = 0; i < 100; ++i) {
-            publisher.process(flow, TENANT_ID);
+            publisher.process(flow, PublisherTestUtils.TENANT_ID);
+        }
+        assertEquals(100, publisher.getAnalyticsCount());
+    }
+
+    public void testInboundEndpointAnalytics() {
+        PublishingFlow flow = new PublishingFlow();
+        flow.addEvent(createPublishingEvent(ComponentType.INBOUNDENDPOINT, PublisherTestUtils.TEST_INBOUND_ENDPOINT));
+        publisher.process(flow, PublisherTestUtils.TENANT_ID);
+        verifySchema(publisher.getAnalyticData(), AnalyticPayloadType.INBOUND_ENDPOINT);
+
+        for (int i = 0; i < 100; ++i) {
+            publisher.process(flow, PublisherTestUtils.TENANT_ID);
         }
         assertEquals(100, publisher.getAnalyticsCount());
     }
@@ -260,10 +254,10 @@ public class ElasticStatisticsTest extends TestCase {
     private PublishingEvent createPublishingEvent(ComponentType componentType, String componentName) {
         PublishingEvent event = new PublishingEvent();
         event.setComponentType(StatisticsConstants.getComponentTypeToString(componentType));
-        event.setFlowId(TEST_FLOW_ID);
+        event.setFlowId(PublisherTestUtils.TEST_FLOW_ID);
         event.setComponentName(componentName);
         long currentTime = Instant.now().toEpochMilli();
-        event.setStartTime(currentTime - STATIC_LATENCY);
+        event.setStartTime(currentTime - PublisherTestUtils.STATIC_LATENCY);
         event.setEndTime(currentTime);
         event.setDuration(event.getEndTime() - event.getStartTime());
         event.setEntryPoint("EP");
@@ -307,6 +301,9 @@ public class ElasticStatisticsTest extends TestCase {
             case SEQUENCE:
                 verifySequencePayload(payloadElement);
                 break;
+            case INBOUND_ENDPOINT:
+                verifyInboundEndpointPayload(payloadElement);
+                break;
             default:
                 assertTrue(payloadElement.isJsonObject());
         }
@@ -319,16 +316,16 @@ public class ElasticStatisticsTest extends TestCase {
 
         JsonObject dataObject = serverInfoElement.getAsJsonObject();
         assertTrue(dataObject.has(ElasticConstants.ServerMetadataFieldDef.HOST_NAME));
-        assertEquals(SERVER_INFO_HOST_NAME,
+        assertEquals(PublisherTestUtils.SERVER_INFO_HOST_NAME,
                 dataObject.get(ElasticConstants.ServerMetadataFieldDef.HOST_NAME).getAsString());
         assertTrue(dataObject.has(ElasticConstants.ServerMetadataFieldDef.SERVER_NAME));
-        assertEquals(SERVER_INFO_SERVER_NAME,
+        assertEquals(PublisherTestUtils.SERVER_INFO_SERVER_NAME,
                 dataObject.get(ElasticConstants.ServerMetadataFieldDef.SERVER_NAME).getAsString());
         assertTrue(dataObject.has(ElasticConstants.ServerMetadataFieldDef.IP_ADDRESS));
-        assertEquals(SERVER_INFO_IP_ADDRESS,
+        assertEquals(PublisherTestUtils.SERVER_INFO_IP_ADDRESS,
                 dataObject.get(ElasticConstants.ServerMetadataFieldDef.IP_ADDRESS).getAsString());
         assertTrue(dataObject.has(ElasticConstants.ServerMetadataFieldDef.PUBLISHER_ID));
-        assertEquals(SERVER_INFO_PUBLISHER_ID,
+        assertEquals(PublisherTestUtils.SERVER_INFO_PUBLISHER_ID,
                 dataObject.get(ElasticConstants.ServerMetadataFieldDef.PUBLISHER_ID).getAsString());
     }
 
@@ -370,19 +367,22 @@ public class ElasticStatisticsTest extends TestCase {
         assertTrue(payload.has(ElasticConstants.EnvelopDef.REMOTE_HOST));
         assertTrue(payload.has(ElasticConstants.EnvelopDef.CONTENT_TYPE));
         assertTrue(payload.has(ElasticConstants.EnvelopDef.HTTP_METHOD));
-        assertEquals(TEST_API_METHOD, payload.get(ElasticConstants.EnvelopDef.HTTP_METHOD).getAsString());
+        assertEquals(PublisherTestUtils.TEST_API_METHOD, payload.get(ElasticConstants.EnvelopDef.HTTP_METHOD)
+                .getAsString());
 
         assertTrue(payload.has(ElasticConstants.EnvelopDef.API_DETAILS));
         assertTrue(payload.get(ElasticConstants.EnvelopDef.API_DETAILS).isJsonObject());
 
         JsonObject apiDetails = payload.get(ElasticConstants.EnvelopDef.API_DETAILS).getAsJsonObject();
         assertTrue(apiDetails.has(ElasticConstants.EnvelopDef.API));
-        assertEquals(TEST_API_NAME, apiDetails.get(ElasticConstants.EnvelopDef.API).getAsString());
+        assertEquals(PublisherTestUtils.TEST_API_NAME, apiDetails.get(ElasticConstants.EnvelopDef.API).getAsString());
         assertTrue(apiDetails.has(ElasticConstants.EnvelopDef.SUB_REQUEST_PATH));
         assertTrue(apiDetails.has(ElasticConstants.EnvelopDef.API_CONTEXT));
-        assertEquals(TEST_API_CONTEXT, apiDetails.get(ElasticConstants.EnvelopDef.API_CONTEXT).getAsString());
+        assertEquals(PublisherTestUtils.TEST_API_CONTEXT, apiDetails
+                .get(ElasticConstants.EnvelopDef.API_CONTEXT).getAsString());
         assertTrue(apiDetails.has(ElasticConstants.EnvelopDef.METHOD));
-        assertEquals(TEST_API_METHOD, apiDetails.get(ElasticConstants.EnvelopDef.METHOD).getAsString());
+        assertEquals(PublisherTestUtils.TEST_API_METHOD, apiDetails.get(ElasticConstants.EnvelopDef.METHOD)
+                .getAsString());
     }
 
     private void verifyEndpointPayload(JsonElement payloadElement) {
@@ -395,7 +395,10 @@ public class ElasticStatisticsTest extends TestCase {
         assertTrue(payload.has(ElasticConstants.EnvelopDef.ENDPOINT_DETAILS));
         JsonObject endpointDetails = payload.get(ElasticConstants.EnvelopDef.ENDPOINT_DETAILS).getAsJsonObject();
         assertTrue(endpointDetails.has(ElasticConstants.EnvelopDef.ENDPOINT_NAME));
-        assertEquals(TEST_ENDPOINT_NAME, endpointDetails.get(ElasticConstants.EnvelopDef.ENDPOINT_NAME).getAsString());
+        assertEquals(PublisherTestUtils.TEST_ENDPOINT_NAME,
+                endpointDetails.get(ElasticConstants.EnvelopDef.ENDPOINT_NAME).getAsString());
+        assertEquals(PublisherTestUtils.TEST_INBOUND_ENDPOINT,
+                endpointDetails.get(ElasticConstants.EnvelopDef.INBOUND_ENDPOINT_NAME).getAsString());
     }
 
     private void verifyProxyServicePayload(JsonElement payloadElement) {
@@ -406,8 +409,22 @@ public class ElasticStatisticsTest extends TestCase {
         verifyCommonPayloadFields(payload);
 
         assertTrue(payload.has(ElasticConstants.EnvelopDef.PROXY_SERVICE_DETAILS));
-        JsonObject proxyServiceDetails = payload.get(ElasticConstants.EnvelopDef.PROXY_SERVICE_DETAILS).getAsJsonObject();
+        JsonObject proxyServiceDetails = payload.get(ElasticConstants.EnvelopDef.PROXY_SERVICE_DETAILS)
+                .getAsJsonObject();
         assertTrue(proxyServiceDetails.has(ElasticConstants.EnvelopDef.PROXY_SERVICE_NAME));
+    }
+
+    private void verifyInboundEndpointPayload(JsonElement payloadElement) {
+        assertNotNull(payloadElement);
+        assertTrue(payloadElement.isJsonObject());
+
+        JsonObject payload = payloadElement.getAsJsonObject();
+        verifyCommonPayloadFields(payload);
+
+        assertTrue(payload.has(ElasticConstants.EnvelopDef.INBOUND_ENDPOINT_DETAILS));
+        JsonObject inboundEndpointDetails = payload.get(ElasticConstants.EnvelopDef.INBOUND_ENDPOINT_DETAILS)
+                .getAsJsonObject();
+        assertTrue(inboundEndpointDetails.has(ElasticConstants.EnvelopDef.INBOUND_ENDPOINT_NAME));
     }
 
     private void verifyCommonPayloadFields(JsonObject payload) {
@@ -416,7 +433,7 @@ public class ElasticStatisticsTest extends TestCase {
         assertTrue(payload.has(ElasticConstants.EnvelopDef.FAULT_RESPONSE));
         assertTrue(payload.has(ElasticConstants.EnvelopDef.FAILURE));
         assertTrue(payload.has(ElasticConstants.EnvelopDef.LATENCY));
-        assertEquals(STATIC_LATENCY, payload.get(ElasticConstants.EnvelopDef.LATENCY).getAsInt());
+        assertEquals(PublisherTestUtils.STATIC_LATENCY, payload.get(ElasticConstants.EnvelopDef.LATENCY).getAsInt());
         assertTrue(payload.has(ElasticConstants.EnvelopDef.METADATA));
     }
 
@@ -441,6 +458,7 @@ public class ElasticStatisticsTest extends TestCase {
         ENDPOINT,
         API,
         SEQUENCE,
+        INBOUND_ENDPOINT,
         NON_STANDARD
     }
 }
